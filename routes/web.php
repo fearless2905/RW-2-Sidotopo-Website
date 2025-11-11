cc8fa676dc0557036621c5549ddb1130ce5fbd43
Alif Bayu Ammarizky <alifbayuku@gmail.com>
2025-11-11 16:24:55 +0700

    add routes blog

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,41 @@ Route::get('/blog-single', function () {
     return view('blog-single');
 })->name('blog-single');
 
+Route::get('/blog/pembukaan-kkn-sdgs', function () {
+    return view('blog-pembukaan-kkn');
+})->name('blog-pembukaan-kkn');
+
+
+
+Route::get('/blog/tangkahan-elephant', function () {
+    return view('blog-tangkahan');
+})->name('blog-tangkahan');
+
+Route::get('/blog/wakatobi-snorkeling', function () {
+    return view('blog-wakatobi');
+})->name('blog-wakatobi');
+
+Route::get('/blog/posyandu-rw2', function () {
+    return view('blog-posyandu');
+})->name('blog-posyandu');
+
+Route::get('/blog/senam-bersama', function () {
+    return view('blog-senam');
+})->name('blog-senam');
+
+Route::get('/blog/bank-sampah', function () {
+    return view('blog-bank-sampah');
+})->name('blog-bank-sampah');
+
+Route::get('/blog/kebun-asri', function () {
+    return view('blog-kebun-asri');
+})->name('blog-kebun-asri');
+
+Route::get('/blog/mural-daun-pohon', function () {
+    return view('blog-mural');
+})->name('blog-mural');
+
+Route::get('/blog/ecobrick-rw2', function () {
+    return view('blog-ecobrick');
+})->name('blog-ecobrick');
+
